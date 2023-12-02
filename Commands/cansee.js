746c904b57c366279115a37dfbe9e56e300f802b
DewandaArya <94660808+DewandaArya@users.noreply.github.com>
2023-12-02 06:35:03 +0000

    canSee fixed.

--- Commands/cansee.js
+++ Commands/cansee.js
@@ -1,4 +1,22 @@
-exports.run
 
+exports.run = async(client, msg, args, bot) => {
+const result = /canSee (-?[0-9]+),(-?[0-9]+),(-?[0-9]+)/.exec(msg.content)
+  if (result) {
+    canSee(new Vec3(result[1], result[2], result[3]))
+    return
+  };
 
-exports.name = "cansee";
+bot.chat("That's nice");
+  };
+
+exports.name = "can see";
+
+  function canSee (pos) {
+    const block = bot.blockAt(pos)
+    const r = bot.canSeeBlock(block)
+    if (r) {
+      bot.chat(`I can see the block of ${block.displayName} at ${pos}`)
+    } else {
+      bot.chat(`I cannot see the block of ${block.displayName} at ${pos}`);
+    };
+  };
